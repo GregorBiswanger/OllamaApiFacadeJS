948bcac5cc5a395d303b8a0425225880e479e859
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2025-02-17 03:01:37 +0100

    Migrate new ESLint configuration in ES module format; update Prettier configuration with 'endOfLine' option

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'no-console': 'warn',
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+    },
+  },
+];
